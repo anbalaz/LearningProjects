95108e9d063c8f609151c32a5162b5f7df9b237c
anbalaz <anbalaz@gmail.com>
2018-01-13 21:28:12 +0100

    add a beginning, resolve problem with stopping the program

--- TextGame/src/App_TextGame.java
+++ TextGame/src/App_TextGame.java
@@ -3,19 +3,34 @@ import java.util.Scanner;
 public class App_TextGame {
 	public static void main(String[] args) {
 
+		boolean commandHandled = false;
 		Scanner sc = new Scanner(System.in);
 		String command = TextResources.room1description;
+		String FirstRoomDescription= TextResources.room1description;
 
-		// System.out.println("You're in some kind of room, you're head is exploding
-		// from pain.\nIt is possible that you are blind. Do you wanna try to open your
-		// eyes? y/n");
-
-		System.out.println("You're in the room with two doors which door do you enter left or right?");
-		boolean commandHandled = false;
+		
+		System.out.println(FirstRoomDescription);
+		
+		while(!commandHandled) {
+			command= sc.nextLine();
+			if (command.equals("y")){
+				System.out.println("You're in the room with two doors which door do you enter left or right?");
+				commandHandled = true;
+				}
+			if (command.equals("n")) {
+				System.out.println("You die by starving, because you are coward");
+				//return;
+				System.exit(0);
+			}
+			
+		}
+					
+		System.out.println("Hello");
 		
 		while (!commandHandled) {
 			command = sc.next();
 			if (command.equals("right")) {
+				
 				System.out.println("bllaaaa");
 				commandHandled = true;
 			}
@@ -23,10 +38,12 @@ public class App_TextGame {
 				System.out.println("blleeee");
 				commandHandled = true;
 			}
+			if (command.equals("stay")) {
+				System.out.println("You die by starving");
+			}
 		}
 
 
-		String swedrftgyh = TextResources.room1description;
 
 	}
 }
--- TextGame/src/TextResources.java
+++ TextGame/src/TextResources.java
@@ -1,5 +1,5 @@
 
 public class TextResources {
-	public static String room1description="You're in the room" ;
+	public static String room1description="You're in some kind of room, you're head is exploding from pain.\nIt is possible that you are blind. Do you wanna try to open your eyes? y/n" ;
 
 }
