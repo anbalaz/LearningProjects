b525665c90415f5e350afc3246aac01ec4668fef
anbalaz <anbalaz@gmail.com>
2018-02-04 17:46:18 +0100

    Testing

---
+++ Faktorial/src/Testing.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Testing {
+	public static void main(String[] args) {
+		List<Integer> values = new ArrayList<Integer>();
+
+		values.add(5);
+		values.add(7);
+		values.add(2);
+		values.add(1);
+		values.add(23);
+		
+		lastValue(values);
+		
+		System.out.println(values.get(values.size()-1));
+		
+		
+	}
+	
+	private static void lastValue(List<Integer>list) {
+		list.add(5);
+		list.add(3);
+		list.add(2);
+		list.add(9);
+		list.add(3,17);
+		
+		for(int i=0;i<50;i++) {
+			list.add(0,i);
+						
+			System.out.println(list.get(i));
+			}
+		
+	}
+
+}
