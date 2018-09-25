7835bfcc734ccc0c9e350b6e71295901ca2f3720
anbalaz <anbalaz@gmail.com>
2018-09-25 23:08:37 +0200

    creating constructor, atributes, toString in class GameObject

--- Room_text_game/src/TheGame.java
+++ Room_text_game/src/TheGame.java
@@ -1,7 +1,11 @@
+import objects.GameObject;
+
 public class TheGame {
 
     public void runTheGame(){
         System.out.println("The game is running");
+        GameObject object= new GameObject("hovno",true,true);
+        System.out.println(object.toString());
 
     }
 }
